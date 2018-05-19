22865e2997d7007ac46cd2996347f86f36094456
eriktate <erik.tate.r@gmail.com>
2018-05-19 12:30:21 -0400

    Fixing bugs with Error types

--- error.go
+++ error.go
@@ -15,7 +15,7 @@ type Error struct {
 func (e Error) Error() string {
 	var errorText string
 	if e.Field != "" {
-		errorText = fmt.Sprintf("With field %s, ", e.Field)
+		errorText = fmt.Sprintf("With field '%s', ", e.Field)
 	}
 
 	return errorText + e.Reason
@@ -23,13 +23,15 @@ func (e Error) Error() string {
 
 // Errors is the full error response that Linode returns on 4xx and 5xx status codes. It
 // aliases a slice of Error structs so that the go error interface can be fulfilled.
-type Errors []Error
+type Errors struct {
+	Errors []Error `json:"errors"`
+}
 
 // Error implements the go error interface for Errors.
 func (e Errors) Error() string {
-	errorTexts := make([]string, len(e))
+	errorTexts := make([]string, len(e.Errors))
 
-	for i, err := range e {
+	for i, err := range e.Errors {
 		errorTexts[i] = err.Error()
 	}
 

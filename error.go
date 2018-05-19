1d50a28d7f93cea47299328fad5093a3ff9e7ced
eriktate <erik.tate.r@gmail.com>
2018-05-19 12:25:20 -0400

    Added error types for unmarshaling Linode error response. Updated client so that it attempts to unmarshal into a Linode error and return that response

---
+++ error.go
@@ -0,0 +1,37 @@
+package lingo
+
+import (
+	"fmt"
+	"strings"
+)
+
+// An Error is the structured error type that Linode returns on 4xx and 5xx status codes.
+type Error struct {
+	Field  string `json:"field,omitempty"`
+	Reason string `json:"reason"`
+}
+
+// Error implements the go error interface for Errors.
+func (e Error) Error() string {
+	var errorText string
+	if e.Field != "" {
+		errorText = fmt.Sprintf("With field %s, ", e.Field)
+	}
+
+	return errorText + e.Reason
+}
+
+// Errors is the full error response that Linode returns on 4xx and 5xx status codes. It
+// aliases a slice of Error structs so that the go error interface can be fulfilled.
+type Errors []Error
+
+// Error implements the go error interface for Errors.
+func (e Errors) Error() string {
+	errorTexts := make([]string, len(e))
+
+	for i, err := range e {
+		errorTexts[i] = err.Error()
+	}
+
+	return strings.Join(errorTexts, "\n")
+}

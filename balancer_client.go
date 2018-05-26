68b634586579840ed5a3fe0ece82ed9ad444f6a0
eriktate <erik.tate.r@gmail.com>
2018-05-26 01:10:41 -0400

    Fleshing out all API calls for node balancers. Just need to add comments, validation functions for enums, and unit tests

--- balancer_client.go
+++ balancer_client.go
@@ -48,14 +48,6 @@ func (c BalancerClient) ViewNodeBalancer(id uint) (NodeBalancer, error) {
 	return balancer, nil
 }
 
-func (c BalancerClient) DeleteNodeBalancer(id uint) error {
-	if _, err := c.api.Delete(fmt.Sprintf("nodebalancers/%d", id)); err != nil {
-		return errors.Wrap(err, "failed to make request for DeleteNodeBalancer")
-	}
-
-	return nil
-}
-
 func (c BalancerClient) CreateNodeBalancer(request CreateBalancerRequest) (NodeBalancer, error) {
 	var created NodeBalancer
 
@@ -94,4 +86,174 @@ func (c BalancerClient) UpdateNodeBalancer(request UpdateBalancerRequest) (NodeB
 	}
 
 	return created, nil
+}
+
+func (c BalancerClient) DeleteNodeBalancer(id uint) error {
+	if _, err := c.api.Delete(fmt.Sprintf("nodebalancers/%d", id)); err != nil {
+		return errors.Wrap(err, "failed to make request for DeleteNodeBalancer")
+	}
+
+	return nil
+}
+
+func (c BalancerClient) ListNodeBalancerConfigs(balancerID uint) ([]BalancerConfig, error) {
+	data, err := c.api.Get(fmt.Sprintf("nodebalancers/%d/configs", balancerID))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to make request for ListNodeBalancerConfigs")
+	}
+
+	var results Results
+	if err := json.Unmarshal(data, &results); err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal ListNodeBalancerConfigs response")
+	}
+
+	var configs []BalancerConfig
+	if err := json.Unmarshal(results.Data, &configs); err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal ListNodeBalancerConfigs data")
+	}
+
+	return configs, nil
+}
+
+func (c BalancerClient) ViewNodeBalancerConfig(balancerID, configID uint) (BalancerConfig, error) {
+	var config BalancerConfig
+	data, err := c.api.Get(fmt.Sprintf("nodebalancers/%d/configs/%d", balancerID, configID))
+	if err != nil {
+		return config, errors.Wrap(err, "failed to make request for ViewNodeBalancerConfig")
+	}
+
+	if err := json.Unmarshal(data, &config); err != nil {
+		return config, errors.Wrap(err, "failed to decode ViewNodeBalancerConfig response")
+	}
+
+	return config, nil
+}
+
+func (c BalancerClient) CreateNodeBalancerConfig(req CreateBalancerConfigRequest) (BalancerConfig, error) {
+	var created BalancerConfig
+
+	payload, err := json.Marshal(&req)
+	if err != nil {
+		return created, errors.Wrap(err, "failed to marshal request for CreateNodeBalancerConfig")
+	}
+
+	data, err := c.api.Post(fmt.Sprintf("nodebalancers/%d/configs", req.NodeBalancerID), payload)
+	if err != nil {
+		return created, errors.Wrap(err, "failed to make request for CreateNodeBalancerConfig")
+	}
+
+	if err := json.Unmarshal(data, &created); err != nil {
+		return created, errors.Wrap(err, "failed to unmarshal CreateNodeBalancerConfig response")
+	}
+
+	return created, nil
+}
+
+func (c BalancerClient) UpdateNodeBalancerConfig(req UpdateBalancerConfigRequest) (NodeBalancer, error) {
+	var updated NodeBalancer
+
+	payload, err := json.Marshal(&req)
+	if err != nil {
+		return updated, errors.Wrap(err, "failed to marshal request for UpdateNodeBalancerConfig")
+	}
+
+	data, err := c.api.Put(fmt.Sprintf("nodebalancers/%d/configs/%d", req.NodeBalancerID, req.ID), payload)
+	if err != nil {
+		return updated, errors.Wrap(err, "failed to make request for UpdateNodeBalancerConfig")
+	}
+
+	if err := json.Unmarshal(data, &updated); err != nil {
+		return updated, errors.Wrap(err, "failed to unmarshal UpdateNodeBalancerConfig response")
+	}
+
+	return updated, nil
+}
+
+func (c BalancerClient) DeleteNodeBalancerConfig(balancerID, configID uint) error {
+	if _, err := c.api.Delete(fmt.Sprintf("nodebalancers/%d/configs/%d", balancerID, configID)); err != nil {
+		return errors.Wrap(err, "failed to make request for DeleteNodeBalancerConfig")
+	}
+
+	return nil
+}
+
+func (c BalancerClient) ListNodes(balancerID, configID uint) ([]Node, error) {
+	data, err := c.api.Get(fmt.Sprintf("nodebalancers/%d/configs/%d/nodes", balancerID, configID))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to make request for ListNodes")
+	}
+
+	var results Results
+	if err := json.Unmarshal(data, &results); err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal ListNodes response")
+	}
+
+	var nodes []Node
+	if err := json.Unmarshal(results.Data, &nodes); err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal ListNodes data")
+	}
+
+	return nodes, nil
+}
+
+func (c BalancerClient) ViewNode(balancerID, configID, nodeID uint) (Node, error) {
+	var node Node
+	data, err := c.api.Get(fmt.Sprintf("nodebalancers/%d/configs/%d/nodes/%d", balancerID, configID, nodeID))
+	if err != nil {
+		return node, errors.Wrap(err, "failed to make request for ViewNode")
+	}
+
+	if err := json.Unmarshal(data, &node); err != nil {
+		return node, errors.Wrap(err, "failed to decode ViewNode response")
+	}
+
+	return node, nil
+}
+
+func (c BalancerClient) CreateNode(req CreateNodeRequest) (Node, error) {
+	var created Node
+
+	payload, err := json.Marshal(&req)
+	if err != nil {
+		return created, errors.Wrap(err, "failed to marshal request for CreateNode")
+	}
+
+	data, err := c.api.Post(fmt.Sprintf("nodebalancers/%d/configs/%d/nodes", req.BalancerID, req.ConfigID), payload)
+	if err != nil {
+		return created, errors.Wrap(err, "failed to make request for CreateNode")
+	}
+
+	if err := json.Unmarshal(data, &created); err != nil {
+		return created, errors.Wrap(err, "failed to unmarshal CreateNode response")
+	}
+
+	return created, nil
+}
+
+func (c BalancerClient) UpdateNode(req UpdateNodeRequest) (Node, error) {
+	var updated Node
+
+	payload, err := json.Marshal(&req)
+	if err != nil {
+		return updated, errors.Wrap(err, "failed to marshal request for UpdateNode")
+	}
+
+	data, err := c.api.Put(fmt.Sprintf("nodebalancers/%d/configs/%d/nodes/%d", req.BalancerID, req.ConfigID, req.ID), payload)
+	if err != nil {
+		return updated, errors.Wrap(err, "failed to make request for UpdateNode")
+	}
+
+	if err := json.Unmarshal(data, &updated); err != nil {
+		return updated, errors.Wrap(err, "failed to unmarshal UpdateNode response")
+	}
+
+	return updated, nil
+}
+
+func (c BalancerClient) DeleteNode(balancerID, configID, nodeID uint) error {
+	if _, err := c.api.Delete(fmt.Sprintf("nodebalancers/%d/configs/%d/nodes/%d", balancerID, configID, nodeID)); err != nil {
+		return errors.Wrap(err, "failed to make request for DeleteNode")
+	}
+
+	return nil
 }
